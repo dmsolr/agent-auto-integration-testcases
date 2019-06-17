efb9a0f6784f872dc48b8757fde2cde9688900ef
dmsolr <zteny@foxmail.com>
2019-06-18 01:28:54 +0800

    fix things

--- solr-scenario/docker/startup.sh
+++ solr-scenario/docker/startup.sh
@@ -6,7 +6,7 @@ if [ -f "${AGENT_FILE_PATH}/skywalking-agent.jar" ]; then
     -Dskywalking.collector.grpc_channel_check_interval=2 \
     -Dskywalking.collector.app_and_service_register_check_interval=2 \
     -Dcollector.discovery_check_interval=2 -Dskywalking.collector.backend_service=${COLLECTOR_SERVER} \
-    -Dskywalking.agent.service_name=solrj-scenario -Xms256m -Xmx256m -XX:PermSize=64M -XX:MaxPermSize=64"
+    -Dskywalking.agent.service_name=solr-client-scenario -Xms256m -Xmx256m -XX:PermSize=64M -XX:MaxPermSize=64"
 fi
 
 java $SOLRJ_OPTS -Dsolr.host=${SOLR_HOST} -jar /usr/local/skywalking-solr-scenario/solr-scenario.jar
